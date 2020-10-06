2797f0e476db52cb0bd141c0613638571ea9c1f5
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2020-10-05 23:14:03 -0400

    added linear gradient, logo and company name to splash screen

--- hydris/lib/splashscreen.dart
+++ hydris/lib/splashscreen.dart
@@ -10,25 +10,37 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(186, 4, 34, 1.0),
-            Color.fromRGBO(36, 11, 54, 1.0)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )),
-        child: Center(
-          child: Container(
-            child: Image.asset(
-              'assets/launcher/icon_foreground.png',
-              height: 250,
-              width: 250,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(186, 4, 26, 1.0),
+              Color.fromRGBO(36, 11, 36, 1.0)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/launcher/icon_foreground.png",
+                    height: 250,
+                    width: 250,
+                  ),
+                  Text(
+                    'H Y D R I S',
+                    style: TextStyle(
+                      fontFamily: "Montserrat",
+                      fontSize: 30.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ),
-      ),
+          )),
       backgroundColor: Colors.transparent,
     );
   }
