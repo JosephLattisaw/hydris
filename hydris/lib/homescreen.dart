85eb26309b488652ed34d6732eb786b43658ba30
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2020-10-14 19:25:31 -0400

    added custom color to display app bar

--- hydris/lib/homescreen.dart
+++ hydris/lib/homescreen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'dart:developer'; //needed for log
 
+Map<int, Color> custom_swatch_color = {
+  //TODO this should match the context file
+  //TODO should optimize this into some type of function
+  50: Color.fromRGBO(0, 48, 71, .1),
+  100: Color.fromRGBO(0, 48, 71, .2),
+  200: Color.fromRGBO(0, 48, 71, .3),
+  300: Color.fromRGBO(0, 48, 71, .4),
+  400: Color.fromRGBO(0, 48, 71, .5),
+  500: Color.fromRGBO(0, 48, 71, .6),
+  600: Color.fromRGBO(0, 48, 71, .7),
+  700: Color.fromRGBO(0, 48, 71, .8),
+  800: Color.fromRGBO(0, 48, 71, .9),
+  900: Color.fromRGBO(0, 48, 71, 1),
+};
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HYDRIS',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFF003065, custom_swatch_color),
       ),
       home: HomeScreenWidget(),
     );
@@ -27,12 +42,6 @@ class HomeScreenState extends State<HomeScreenWidget> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        /* title: const Text(
-          "H Y D R I S",
-          style: TextStyle(
-            fontFamily: "Montserrat",
-          ),
-        ),*/
         title: Container(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -42,7 +51,12 @@ class HomeScreenState extends State<HomeScreenWidget> {
                 height: 30,
                 width: 30,
               ),
-              Text('HYDRIS'),
+              const Text(
+                "H Y D R I S",
+                style: TextStyle(
+                  fontFamily: "Montserrat",
+                ),
+              ),
               SizedBox(
                 height: 30,
                 width: 30,
