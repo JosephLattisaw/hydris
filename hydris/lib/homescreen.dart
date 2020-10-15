2126f6baacbdbbfefa0ff53bd5a422588a6ead6b
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2020-10-15 00:13:19 -0400

    title bar is exactly how we want it

--- hydris/lib/homescreen.dart
+++ hydris/lib/homescreen.dart
@@ -46,7 +46,19 @@ class HomeScreenState extends State<HomeScreenWidget> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
-          children: [Text("test"), Text("test2")],
+          children: [
+            Image.asset(
+              "assets/launcher/icon_foreground.png",
+              fit: BoxFit.scaleDown,
+              height: 35,
+              width: 35,
+            ),
+            Text("H Y D R I S"),
+            SizedBox(
+              height: 35,
+              width: 35,
+            )
+          ],
         ),
         centerTitle: true,
       ),
