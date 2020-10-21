48757ce796ebc306e4d9e86fa74d069540645c1f
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2020-10-21 12:15:40 -0400

    layout is correct on contact screen

--- hydris/lib/contact_screen.dart
+++ hydris/lib/contact_screen.dart
@@ -46,11 +46,7 @@ class _ContactScreenState extends State<ContactScreen> {
               child: Column(
                 children: [
                   Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        border: Border(
-                            top: BorderSide(color: Colors.black, width: 3.0))),
+                    color: Theme.of(context).primaryColor,
                     child: Column(
                       children: [
                         Text(
@@ -65,7 +61,7 @@ class _ContactScreenState extends State<ContactScreen> {
                           style: TextStyle(color: Colors.white, fontSize: 14),
                         ),
                         Container(
-                          color: Colors.transparent,
+                          color: Theme.of(context).primaryColor,
                           child: Row(
                             children: [
                               Expanded(
@@ -77,31 +73,10 @@ class _ContactScreenState extends State<ContactScreen> {
                                   child: Column(
                                     children: [
                                       Icon(Icons.location_on),
-                                      Text("123 Street Ave"),
+                                      Text("123 Street Ave.")
                                     ],
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                width: 2.0,
-                              ),
-                              Expanded(
-                                child: RaisedButton(
-                                  elevation: 40.0,
-                                  color: Colors.orange[900],
-                                  onPressed: () {},
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5.0)),
-                                  child: Column(
-                                    children: [
-                                      Icon(Icons.phone),
-                                      Text("1-800-HYDRIS"),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 2.0,
                               ),
                               Expanded(
                                 child: RaisedButton(
@@ -111,30 +86,37 @@ class _ContactScreenState extends State<ContactScreen> {
                                       borderRadius: BorderRadius.circular(5.0)),
                                   child: Column(
                                     children: [
-                                      Icon(Icons.mail),
-                                      Text("email@hydris"),
+                                      Icon(Icons.location_on),
+                                      Text("123 Street Ave.")
                                     ],
                                   ),
                                 ),
                               ),
+                              Expanded(
+                                child: RaisedButton(
+                                  color: Colors.orange[900],
+                                  onPressed: () {},
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5.0)),
+                                  child: Column(
+                                    children: [
+                                      Icon(Icons.location_on),
+                                      Text("123 Street Ave.")
+                                    ],
+                                  ),
+                                ),
+                              )
                             ],
                           ),
-                        ),
+                        )
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
                   ),
                   Text(
                     "Send us a message",
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
                   Container(
-                    width: MediaQuery.of(context).size.width * .75,
                     child: Column(
                       children: [
                         TextField(
@@ -156,147 +138,149 @@ class _ContactScreenState extends State<ContactScreen> {
                           ),
                           style: TextStyle(fontSize: 16.0, color: Colors.black),
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
                         TextField(
                           decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.blue[900], width: 2.0)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.orange[900], width: 2.0)),
-                              fillColor: Colors.white,
-                              labelText: "Email Address",
-                              prefixIcon: Icon(
-                                Icons.mail,
-                                color: Colors.blue[900],
-                              ),
-                              //filled: true,
-                              contentPadding: EdgeInsets.all(0)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.blue[900], width: 2.0)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.orange[900], width: 2.0)),
+                            fillColor: Colors.white,
+                            labelText: "E-mail Address",
+                            prefixIcon: Icon(
+                              Icons.mail,
+                              color: Colors.blue[900],
+                            ),
+                            //filled: true,
+                            contentPadding: EdgeInsets.all(0),
+                          ),
                           style: TextStyle(fontSize: 16.0, color: Colors.black),
                         ),
-                        SizedBox(
-                          height: 5,
-                        ),
                         TextField(
                           decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.blue[900], width: 2)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.orange[900], width: 2)),
-                              fillColor: Colors.white,
-                              labelText: "Phone Number",
-                              prefixIcon: Icon(
-                                Icons.phone_android,
-                                color: Colors.blue[900],
-                              ),
-                              //filled: true,
-                              contentPadding: EdgeInsets.all(0)),
-                          style: TextStyle(fontSize: 16.0, color: Colors.black),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.multiline,
-                          decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.blue[900], width: 2)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.orange[900], width: 2)),
-                              fillColor: Colors.white,
-                              labelText: "Message",
-                              prefixIcon: Icon(
-                                Icons.edit,
-                                color: Colors.blue[900],
-                              ),
-                              //filled: true,
-                              contentPadding: EdgeInsets.all(0)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.blue[900], width: 2.0)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.orange[900], width: 2.0)),
+                            fillColor: Colors.white,
+                            labelText: "Phone Number",
+                            prefixIcon: Icon(
+                              Icons.phone_android,
+                              color: Colors.blue[900],
+                            ),
+                            contentPadding: EdgeInsets.all(0),
+                          ),
                           style: TextStyle(fontSize: 16.0, color: Colors.black),
                         ),
                       ],
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      RaisedButton(
-                        onPressed: () {
-                          setState(() {
-                            toggleServicesIndex = 0;
-                          });
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.orange[900], width: 2.0)),
-                        child: Text("Quote"),
-                        color: toggleServicesIndex == 0
-                            ? Colors.orange[900]
-                            : Colors.white,
+                  Expanded(
+                    child: Container(
+                      height: double.infinity,
+                      child: TextField(
+                        keyboardType: TextInputType.multiline,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.blue[900], width: 2.0)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Colors.orange[900], width: 2.0)),
+                          labelText: "Message",
+                          prefixIcon: Icon(
+                            Icons.edit,
+                            color: Colors.blue[900],
+                          ),
+                        ),
+                        maxLines: 20,
                       ),
-                      RaisedButton(
-                        onPressed: () {
-                          setState(() {
-                            toggleServicesIndex = 1;
-                          });
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.orange[900], width: 2.0)),
-                        child: Text("Proposal"),
-                        color: toggleServicesIndex == 1
-                            ? Colors.orange[900]
-                            : Colors.white,
-                      ),
-                      RaisedButton(
-                        onPressed: () {
-                          setState(() {
-                            toggleServicesIndex = 2;
-                          });
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.orange[900], width: 2.0)),
-                        child: Text("Questions"),
-                        color: toggleServicesIndex == 2
-                            ? Colors.orange[900]
-                            : Colors.white,
-                      ),
-                      RaisedButton(
-                        onPressed: () {
-                          setState(() {
-                            toggleServicesIndex = 3;
-                          });
-                        },
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.orange[900], width: 2.0)),
-                        child: Text("Apply"),
-                        color: toggleServicesIndex == 3
-                            ? Colors.orange[900]
-                            : Colors.white,
-                      ),
-                    ],
-                  ),
-                  RaisedButton(
-                    elevation: 20,
-                    onPressed: () {},
-                    child: Text(
-                      "Send Message",
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
                     ),
-                    color: Colors.orange[900],
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            RaisedButton(
+                              onPressed: () {
+                                setState(() {
+                                  toggleServicesIndex = 0;
+                                });
+                              },
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.orange[900], width: 2.0)),
+                              child: Text("Quote"),
+                              color: toggleServicesIndex == 0
+                                  ? Colors.orange[900]
+                                  : Colors.white,
+                            ),
+                            RaisedButton(
+                              onPressed: () {
+                                setState(() {
+                                  toggleServicesIndex = 0;
+                                });
+                              },
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.orange[900], width: 2.0)),
+                              child: Text("Quote"),
+                              color: toggleServicesIndex == 0
+                                  ? Colors.orange[900]
+                                  : Colors.white,
+                            ),
+                            RaisedButton(
+                              onPressed: () {
+                                setState(() {
+                                  toggleServicesIndex = 0;
+                                });
+                              },
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.orange[900], width: 2.0)),
+                              child: Text("Quote"),
+                              color: toggleServicesIndex == 0
+                                  ? Colors.orange[900]
+                                  : Colors.white,
+                            ),
+                            RaisedButton(
+                              onPressed: () {
+                                setState(() {
+                                  toggleServicesIndex = 0;
+                                });
+                              },
+                              shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Colors.orange[900], width: 2.0)),
+                              child: Text("Quote"),
+                              color: toggleServicesIndex == 0
+                                  ? Colors.orange[900]
+                                  : Colors.white,
+                            ),
+                          ],
+                        ),
+                        RaisedButton(
+                          elevation: 20,
+                          onPressed: () {},
+                          child: Text(
+                            "Send Message",
+                            style: TextStyle(
+                                fontSize: 17, fontWeight: FontWeight.bold),
+                          ),
+                          color: Colors.orange[900],
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
