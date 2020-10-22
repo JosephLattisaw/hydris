bb542060bf2dd3c7c32dbcc544197c4bd411206c
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2020-10-22 01:31:06 -0400

    added american flag background to background of about us page

--- hydris/lib/about_us_screen.dart
+++ hydris/lib/about_us_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hydris/app_bar.dart';
+import 'package:hydris/main_drawer.dart';
 
 class AboutUsScreen extends StatefulWidget {
   @override
@@ -8,6 +10,35 @@ class AboutUsScreen extends StatefulWidget {
 class _AboutUsScreenState extends State<AboutUsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: HydrisAppBar(),
+        drawer: MainDrawer(),
+        body: Stack(
+          children: [
+            Container(
+              child: Image.asset(
+                "assets/american-flag-3001893.png",
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+                color: Colors.redAccent[700],
+                colorBlendMode: BlendMode.lighten,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                Colors.blue[900].withOpacity(0.9),
+                Colors.black.withOpacity(.6),
+              ])),
+            ),
+            Container(
+              decoration: BoxDecoration(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
